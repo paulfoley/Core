b6c6cd10c858608403e07e80636654cf5540b5e6
Adam Holt <Adam Holt>
2016-03-13 17:53:24 -0600

    Fixed variable definitions in create_instance.  Added argument.

--- app/controllers/core_controller.rb
+++ app/controllers/core_controller.rb
@@ -2,49 +2,45 @@ class CoreController < ApplicationController
   def run
   end
 
-  def create_instance(element)
+  helper_method :create_instance
+
+  def create_instance(element, url_code, org_name)
     require 'net/http'
     require 'json'
 
     @path = "https://api.cloud-elements.com/elements/api-v2/elements/#{element}/oauth/url"
-    @apiKey = '3MVG9uudbyLbNPZM9tgYt1cA4yglnVMRyvCK01x2K8j3Qo1I.MkpK2mm8xTrBdwNPR8BG1HLN4S_5aRnwYPUc'
-    @apiSecret = '3910440158623989535'
-    @callBack = 'http://127.0.0.1/core/run'
-    response = Net::HTTP::Get(URI.parse("#{@path}?apiKey=#{@apiKey}&apiSecret=#{@apiSecret}&callbackUrl=#{@callBack}&state=sfdc"))
+    @api_key = '3MVG9uudbyLbNPZM9tgYt1cA4yglnVMRyvCK01x2K8j3Qo1I.MkpK2mm8xTrBdwNPR8BG1HLN4S_5aRnwYPUc'
+    @api_secret = '3910440158623989535'
+    @call_back = 'http://127.0.0.1/core/run'
+    response = Net::HTTP::Get(URI.parse("#{@path}?@api_key=#{@api_key}&@api_secret=#{@api_secret}&callbackUrl=#{@call_back}&state=sfdc"))
 
     response_hash = JSON.parse(response)
 
     @element = response_hash['element']
-    @oauthUrl = response_hash['oauthUrl']
+    @oauth_url = response_hash['@oauth_url']
 
-    @orgName = 'Acme'
-    @userSecret = 'ijHDATuDStgbpAlvXbNTn9gnLIblO5OtiHhbpER3S60='
-    @orgSecret = '52d24ebf350ceeb29afef2768b668852'
+    @org_name = org_name
+    @user_secret = 'ijHDATuDStgbpAlvXbNTn9gnLIblO5OtiHhbpER3S60='
+    @org_secret = '52d24ebf350ceeb29afef2768b668852'
 
-    @path = "https://api.cloud-elements.com/elements/api-v2/instances"
+    @path = 'https://console.cloud-elements.com/elements/api-v2/instances'
     @body = {
-        "element" => {"key" => @element},
-        "providerData" => {"code" => "<Code_On_The_Return_URL>"},
-        "configuration" => {
-        "oauth.callback.url" => "http://127.0.0.1/core/run",
-        "oauth.api.key" => @apiKey,
-        "oauth.api.secret" => @apiSecret},
-        "name" => "#{@orgName} SalesForce"
+        :element => {:key => @element},
+        :providerData => {:code => url_code},
+        :configuration => {
+        'oauth.callback.url' => @call_back,
+        'oauth.api.key' => @api_key,
+        'oauth.api.secret' => @api_secret},
+        :name => @org_name
     }.to_json
 
     @headers = {
-        "Authorization" => {
-            "User" => @userSecret,
-            "Organization" => @orgSecret
-        },
-
-        "Content-Type" => 'application/json'
+        :Authorization => "User => #{@user_secret} Organization #{@org_secret}",
+        'Content-Type' => 'application/json'
     }.to_json
 
     request = Net::HTTP::Post.new(@path, :body => @body, :headers => @headers)
 
-    request_hash = JSON.parse(request)
-
-
+    @request_hash = JSON.parse(request)
   end
 end
