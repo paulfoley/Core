c10a0531115d0250067e6bd96d6da10fdd8a35f5
Adam Holt <Adam Holt>
2016-04-02 15:17:00 -0600

    Updating database models

---
+++ app/models/user_org.rb
@@ -0,0 +1,9 @@
+class UserOrg < ActiveRecord::Base
+  has_many :users
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  devise :omniauthable, :omniauth_providers => [:stripe_connect, :quickbooks]
+end
