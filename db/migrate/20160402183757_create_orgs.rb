f4118ac7aec2020b774fab68f297ea8af6720ca7
Adam Holt <Adam Holt>
2016-04-08 16:03:49 -0600

    Removed old migrations

--- db/migrate/20160402183757_create_orgs.rb
+++
@@ -1,8 +0,0 @@
-class CreateOrgs < ActiveRecord::Migration
-  def change
-    create_table :orgs do |t|
-      t.timestamps null: false
-      t.string :name,               null: false, default: ""
-    end
-  end
-end
--- db/migrate/20160402210958_create_quickbooks_customers.rb
+++
@@ -1,8 +0,0 @@
-class CreateQuickbooksCustomers < ActiveRecord::Migration
-  def change
-    create_table :quickbooks_customers do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
