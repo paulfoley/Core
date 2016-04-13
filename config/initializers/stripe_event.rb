30ad3fc6ced736baca132d15333a50ff80f6a368
Jonathan Song <jonathan.song@colorado.edu>
2016-04-13 15:22:15 -0700

    Removing warning for server on stripe

--- config/initializers/stripe_event.rb
+++ config/initializers/stripe_event.rb
@@ -1,11 +1,3 @@
-Rails.configuration.stripe = {
-    :publishable_key => ENV['PUBLISHABLE_KEY'],
-    :secret_key      => ENV['SECRET_KEY']
-}
-
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
-STRIPE_PUBLIC_KEY = Rails.configuration.stripe[:publishable_key]
-
 StripeEvent.configure do |events|
   events.subscribe 'charge.succeeded', ChargeSucceeded.new
 end
