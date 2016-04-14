fac62066cbe6dc671c0e4936b657858ddd984543
Adam Holt <Adam Holt>
2016-04-13 18:41:26 -0600

    Updated CallbackController

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -2,19 +2,20 @@ class CallbackController < ApplicationController
 
   skip_before_action :verify_authenticity_token
   respond_to :json
-
+  require 'json'
   def receive_data
     render json:{}
 
+    message = params[:message]
+    raw = message[:raw]
+    account = raw[:Account]
+    events = message[:events]
+    name = account[0][:Name]
+    action = events[0][:eventType]
 
-    data = params[:_json]
-    puts data
-
-
-    data.each do |account|
-      Database.create_salesforce_account(account['Name'])
-
-
+    if action == "CREATED"
+      Database.create_salesforce_account(name)
     end
+
   end
 end
