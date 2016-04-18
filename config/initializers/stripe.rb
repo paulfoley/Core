b5fab534b01da0f7d3997765d0f4332337d1ba16
Jonathan Song <jonathan.song@colorado.edu>
2016-04-18 14:15:06 -0700

    Trying to fix the build

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -21,8 +21,27 @@ StripeEvent.configure do |events|
   #
   # REMOVE THIS BLOCK AFTER MAKING YOUR FIXTURES!
   events.event_retriever = lambda { |params|
-    Stripe::Event.construct_from(params.deep_symbolize_keys)
+    event = Stripe::Event.construct_from(params.deep_symbolize_keys)
     puts "within the stripe event retriever in the stripe.rb file"
+    puts event.data.object.id.to_s
+
+=begin
+    charge = Stripe::Charge.retrieve(event.data.object.id.to_s)
+    balance_transaction = Stripe::BalanceTransaction.retrieve(charge.balance_transaction.to_s)
+    @date = Time.at(charge.created)
+    @stripe_customer = stripe_customer.new
+    @stripe_customer.GENERAL_JOURNAL = @date.strftime("%m/%d/%Y")
+    @stripe_customer.Stripe_Sales = charge.amount * -0.01
+    @stripe_customer.Charge_ID = charge.id
+    @stripe_customer.Description = charge.description
+    @stripe_customer.Email = customer.email
+    @stripe_customer.Stripe_Payment_Processing_Fees = balance_transaction.fee * 0.01
+    @stripe_customer.Fees_for_charge_ID = charge.id
+    @stripe_customer.Stripe_Account = balance_transaction.net * 0.01
+    @stripe_customer.Net_for_charge_ID = charge.id
+    @stripe_customer.save
+=end
+
   }
 end
 
