047176dada793cbcee4ee1934431f064c971b0d0
Madison Rockwell <madison.rockwell@colorado.edu>
2016-04-21 17:59:42 -0600

    Instances add their Id to database when created

--- app/models/cloud_elements.rb
+++ app/models/cloud_elements.rb
@@ -68,6 +68,9 @@ class CloudElements
     org = Org.where(name: org_name).select(:name, :salesforce_token, :id).take
     org.update_attributes(:salesforce_token => response_parsed['token'])
 
+    org = Org.where(name: org_name).select(:name, :salesforce_instance_id, :id).take
+    org.update_attributes(:salesforce_instance_id => response_parsed['id'])
+
     self.setup_polling(response_parsed['id'])
 
     if Org.where(name: org_name).select(:quickbooks_token).take.quickbooks_token
@@ -181,7 +184,9 @@ class CloudElements
     org = Org.where(name: org_name).select(:name, :quickbooks_token, :id).take
     org.update_attributes(:quickbooks_token => response_parsed['token'])
 
-    puts org.quickbooks_token
+    org = Org.where(name: org_name).select(:name, :quickbooks_instance_id, :id).take
+    org.update_attributes(:quickbooks_instance_id => response_parsed['id'])
+
     self.setup_polling(response_parsed['id'])
 
     if Org.where(name: org_name).select(:salesforce_token).take.salesforce_token
