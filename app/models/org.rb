e0933cce3c839f2b58e60a98a1018e6cf42a525e
Adam Holt <Adam Holt>
2016-04-22 18:28:16 -0600

    Updated DB write methods

--- app/models/org.rb
+++ app/models/org.rb
@@ -2,4 +2,5 @@ class Org < ActiveRecord::Base
   has_many :users, :dependent => :destroy
   has_many :quickbooks_customers, :dependent => :destroy
   has_many :salesforce_accounts, :dependent => :destroy
+  has_many :stripe_customers, :dependent => :destroy
 end
