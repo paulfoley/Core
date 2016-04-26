199a7bbc5d4cb39bdeb251349bbf986d01da095d
Madison Rockwell <madison.rockwell@colorado.edu>
2016-04-26 01:48:19 -0600

    Salesforce Report bug fix

--- app/models/cloud_elements.rb
+++ app/models/cloud_elements.rb
@@ -515,8 +515,7 @@ class CloudElements
 
     response_parsed.each do |report|
       puts "Checking if report exists"
-      if SalesforceReport.where(org_id: org.id).select(:report_id)
-        
+      if SalesforceReport.where(org_id: org.id).select(:report_id) == report['id']
       else
         puts "Adding report"
         SalesforceReport.create(name: report['name'], report_id: report['id'], org: org)
