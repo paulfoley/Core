6b0509397c481d923e8f3e54b9d5b4adf2c4e20e
Madison Rockwell <madison.rockwell@colorado.edu>
2016-04-25 19:54:52 -0600

    Added Quickbooks report options to database

--- db/migrate/20160426013050_create_quickbooks_reports.rb
+++ db/migrate/20160426013050_create_quickbooks_reports.rb
@@ -6,7 +6,29 @@ class CreateQuickbooksReports < ActiveRecord::Migration
       t.timestamps null: false
     end
 
-    QuickbooksReport.create :report_id => "reportid"
+    QuickbooksReport.create :report_id => "BalanceSheet"
+    QuickbooksReport.create :report_id => "ProfitAndLoss"
+    QuickbooksReport.create :report_id => "ProfitAndLossDetail"
+    QuickbooksReport.create :report_id => "TrialBalance"
+    QuickbooksReport.create :report_id => "CashFlow"
+    QuickbooksReport.create :report_id => "InventoryValuationSummary"
+    QuickbooksReport.create :report_id => "CustomerSales"
+    QuickbooksReport.create :report_id => "ItemSales"
+    QuickbooksReport.create :report_id => "DepartmentSales"
+    QuickbooksReport.create :report_id => "ClassSales"
+    QuickbooksReport.create :report_id => "CustomerIncome"
+    QuickbooksReport.create :report_id => "CustomerBalance"
+    QuickbooksReport.create :report_id => "CustomerBalanceDetail"
+    QuickbooksReport.create :report_id => "AgedReceivables"
+    QuickbooksReport.create :report_id => "AgedReceivableDetail"
+    QuickbooksReport.create :report_id => "VendorBalance"
+    QuickbooksReport.create :report_id => "VendorBalanceDetail"
+    QuickbooksReport.create :report_id => "AgedPayables"
+    QuickbooksReport.create :report_id => "AgedPayableDetail"
+    QuickbooksReport.create :report_id => "VendorExpenses"
+    QuickbooksReport.create :report_id => "AccountList"
+    QuickbooksReport.create :report_id => "GeneralLedger"
+
 
   end
 end
