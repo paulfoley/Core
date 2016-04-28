1e911b2598c4918247a145c35ebe3625b616cb61
Jonathan Song <jonathan.song@colorado.edu>
2016-04-28 14:31:07 -0700

    Live key needed to be used

--- app/stripe_handlers/charge_succeeded.rb
+++ app/stripe_handlers/charge_succeeded.rb
@@ -5,6 +5,7 @@ class ChargeSucceeded < ApplicationController
     stripe_token = event[:user_id]
     puts stripe_token
 
+=begin
     customer_id = event[:data][:object][:customer]
     customer = Stripe::Customer.retrieve(customer_id.to_s)
     #puts customer
@@ -13,9 +14,10 @@ class ChargeSucceeded < ApplicationController
     #puts card
     customer_name = card.name
     puts customer_name
+=end
 
-    #customer_name = Org.where(stripe_token: stripe_token).select(:name).take.name
-    #puts customer_name
+    customer_name = Org.where(stripe_token: stripe_token).select(:name).take.name
+    puts customer_name
 
     amount_paid = event[:data][:object][:amount] * 0.01
     puts amount_paid
