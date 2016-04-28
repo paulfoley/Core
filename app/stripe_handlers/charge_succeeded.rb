3efac5da8e470cf759b7bd839c6f6dc4ddc136e6
Jonathan Song <jonathan.song@colorado.edu>
2016-04-27 18:13:37 -0700

    stripe fix

--- app/stripe_handlers/charge_succeeded.rb
+++ app/stripe_handlers/charge_succeeded.rb
@@ -1,36 +1,30 @@
 class ChargeSucceeded < ApplicationController
 
   def self.webhook(event)
-    #token = Org.where(stripe_token: stripe_token).select(:quickbooks_token).take.quickbooks_token
-    # secret key = SELECT access_token FROM database WHERE user_id = stripe_user_id
-
     stripe_token = event[:user_id]
-    puts stripe_token
+    #puts stripe_token
 
     #customer_name = event[:data][:object][:source][:name]
     #puts customer_name
 
-    #description = event[:data][:object][:description]
-    #puts description
-
     customer_id = event[:data][:object][:customer]
     #puts customer_id
     customer = Stripe::Customer.retrieve(customer_id.to_s)
-    #puts customer.sources.data.name
     #puts customer
 
     #puts event[:data][:object][:source][:id]
-    card_id = event[:data][:object][:source][:id]
-    card = customer.sources.retrieve(card_id.to_s)
+    #card_id = event[:data][:object][:source][:id]
+    #card = customer.sources.retrieve(card_id.to_s)
     #puts card
-    customer_name = card.name
-    puts customer_name
+    customer_name = Org.where(stripe_token: stripe_token).select(:name).take.name
+    #customer_name = card.name
+    #puts customer_name
     #puts customer.sources.data.id
     #card_id = customer.sources.data.id.to_s
     #card = customer.sources.retrieve()
 
     amount_paid = event[:data][:object][:amount] * 0.01
-    puts amount_paid
+    #puts amount_paid
 
     CloudElements.quickbooks_payment(stripe_token, customer_name, amount_paid)
 
