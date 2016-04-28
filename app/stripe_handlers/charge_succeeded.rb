a282be9b785360046d1d86b8a13716f487dd970d
Jonathan Song <jonathan.song@colorado.edu>
2016-04-28 10:48:50 -0700

    stripe fix

--- app/stripe_handlers/charge_succeeded.rb
+++ app/stripe_handlers/charge_succeeded.rb
@@ -1,34 +1,24 @@
 class ChargeSucceeded < ApplicationController
 
   def self.webhook(event)
+    # stripe handler for charge.succeeded event
     stripe_token = event[:user_id]
     puts stripe_token
 
-    #customer_name = event[:data][:object][:source][:name]
-    #puts customer_name
-
-    #customer_id = event[:data][:object][:customer]
-    #puts customer_id
-    #customer = Stripe::Customer.retrieve(customer_id.to_s)
-    #puts customer
-
-    #puts event[:data][:object][:source][:id]
-    #card_id = event[:data][:object][:source][:id]
-    #card = customer.sources.retrieve(card_id.to_s)
-    #puts card
-
-    customer_name = Org.where(stripe_token: stripe_token).select(:name).take.name
+    customer_id = event[:data][:object][:customer]
+    customer = Stripe::Customer.retrieve(customer_id.to_s)
+    card_id = event[:data][:object][:source][:id]
+    card = customer.sources.retrieve(card_id.to_s)
+    customer_name = card.name
     puts customer_name
 
-    #customer_name = card.name
+    #customer_name = Org.where(stripe_token: stripe_token).select(:name).take.name
     #puts customer_name
-    #puts customer.sources.data.id
-    #card_id = customer.sources.data.id.to_s
-    #card = customer.sources.retrieve()
 
     amount_paid = event[:data][:object][:amount] * 0.01
     puts amount_paid
 
+    # call quickbooks_payment to automate data transfer of payment from Stripe to QB
     CloudElements.quickbooks_payment(stripe_token, customer_name.to_s, amount_paid)
 
   end
