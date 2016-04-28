78e31ceddce6d94101179ecb6eac2ae3f854a99d
Jonathan Song <jonathan.song@colorado.edu>
2016-04-28 09:24:11 -0700

    stripe fix

--- app/stripe_handlers/charge_succeeded.rb
+++ app/stripe_handlers/charge_succeeded.rb
@@ -2,21 +2,24 @@ class ChargeSucceeded < ApplicationController
 
   def self.webhook(event)
     stripe_token = event[:user_id]
-    #puts stripe_token
+    puts stripe_token
 
     #customer_name = event[:data][:object][:source][:name]
     #puts customer_name
 
-    customer_id = event[:data][:object][:customer]
+    #customer_id = event[:data][:object][:customer]
     #puts customer_id
-    customer = Stripe::Customer.retrieve(customer_id.to_s)
+    #customer = Stripe::Customer.retrieve(customer_id.to_s)
     #puts customer
 
     #puts event[:data][:object][:source][:id]
     #card_id = event[:data][:object][:source][:id]
     #card = customer.sources.retrieve(card_id.to_s)
     #puts card
+
     customer_name = Org.where(stripe_token: stripe_token).select(:name).take.name
+    #customer_name = Org.where(salesforce_instance_id: 12345).select(:name).take.name
+    puts customer_name
     #customer_name = card.name
     #puts customer_name
     #puts customer.sources.data.id
@@ -24,7 +27,7 @@ class ChargeSucceeded < ApplicationController
     #card = customer.sources.retrieve()
 
     amount_paid = event[:data][:object][:amount] * 0.01
-    #puts amount_paid
+    puts amount_paid
 
     CloudElements.quickbooks_payment(stripe_token, customer_name, amount_paid)
 
