961874c28576ab402d4a3bdd8a44582807701903
Jonathan Song <jonathan.song@colorado.edu>
2016-04-28 19:16:41 -0700

    stripe fix

--- app/stripe_handlers/charge_succeeded.rb
+++ app/stripe_handlers/charge_succeeded.rb
@@ -16,8 +16,15 @@ class ChargeSucceeded < ApplicationController
     puts customer_name
 =end
 
-    customer_name = Org.where(stripe_token: stripe_token).select(:name).take.name
-    puts customer_name
+    # pull customer from database
+    # use email to match with the customer generated when sale made in SF -> invoice in QB
+    customer_id = event[:data][:object][:customer]
+    customer = Stripe::Customer.retrieve(customer_id.to_s)
+    #puts customer
+    stripe_email = customer.email
+    #puts stripe_email
+    customer_name = SalesforceContact.where(email: stripe_email).select(:name).take.name
+    #puts customer_name
 
     amount_paid = event[:data][:object][:amount] * 0.01
     puts amount_paid
