95d7165840d25d703f6b998598d316d74bf9606f
Adam Holt <Adam Holt>
2016-04-29 19:12:45 -0600

    Oops

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -23,8 +23,8 @@ class ElementsController < ApplicationController
       # if params['message'] != "Request failed."
       puts "***** Testing *****"
       puts params['providerMessage']
-        # CloudElements.salesforce_pull_everything(session[:org])
-      end
+        CloudElements.salesforce_pull_everything(session[:org])
+      # end
     elsif @state == "quickbooks"
       CloudElements.quickbooks_instance(session[:org], @uri_params[:oauth_token], @uri_params[:oauth_verifier], @uri_params[:realmId], @uri_params[:dataSource])
       CloudElements.quickbooks_pull_everything(session[:org])
